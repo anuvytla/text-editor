8c00141690dcca7f796365dd4667ce81622b1ae3
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-09-10 22:07:05 -0700

    Add putDB and getDB methods for storing and fetching content from indexDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,35 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // connect to jate DB with version 1.
+  const jateDb = await openDB('jate', 1);
+  // make new transaction...need to specify the DB we are posting to and the data privileges. 
+  const transaction = jateDb.transaction('jate', 'readwrite');
+  // open the object store
+  const objStore = transaction.objectStore('jate');
+  // use the .add() method to pass in content
+  const req = objStore.put({ content: content })
+  // wait for the data add.
+  const res = await req;
+  console.log('data saved to the jateDB', res);
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // connect to jate DB with version 1.
+  const jateDb = await openDB('jate', 1);
+  // make new transaction...need to specify the DB we are posting to and the data privileges. 
+  const transaction = jateDb.transaction('jate', 'readwrite');
+  // open the object store
+  const objStore = transaction.objectStore('jate');
+  // use the .getAll() method to grab all the content in the DB
+  const req = objStore.getAll()
+  // confirm the data was fetched
+  const res = await req;
+  console.log('data fetched from the jateDB', res);
+
+};
 
 initdb();
